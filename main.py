ff31122c3e76b0e145f4279ecfd567fbd7cf7f25
Unarov Aital <unarov.aa@phystech.edu>
2025-03-07 23:49:50 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+from haystack import Document, Pipeline
+from haystack_integrations.document_stores.elasticsearch import ElasticsearchDocumentStore
+
+document_store = ElasticsearchDocumentStore(hosts = "http://localhost:9200")
